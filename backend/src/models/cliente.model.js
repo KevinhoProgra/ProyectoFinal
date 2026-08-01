680147eb0ca167895d859ff17d5c44274ac26dcd
KevinhoProgra <kevin.lizano.vargas@gmail.com>
2026-07-31 20:36:41 -0600

    Removal

--- backend/src/models/cliente.model.js
+++
@@ -1,7 +0,0 @@
-import { crearModeloCrud } from './crud.model.js';
-
-export const clienteModel = crearModeloCrud({
-  tabla: 'clientes',
-  campos: ['cedula', 'nombre', 'apellidos', 'email', 'telefono', 'direccion', 'activo'],
-  orden: 'apellidos, nombre',
-});
--- backend/src/utils/ErrorApi.js
+++
@@ -1,7 +0,0 @@
-// Error con codigo HTTP. El manejador de errores lo convierte en respuesta JSON.
-export class ErrorApi extends Error {
-  constructor(estado, mensaje) {
-    super(mensaje);
-    this.estado = estado;
-  }
-}
